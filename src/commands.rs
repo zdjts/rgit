ba4a8e991d56dd86183ea6829ee80fa128e3d3f3
zeng <z15188247@gmail.com>
2026-03-29 13:48:37 +0800

    feat: 实现 rgit 初始化和对象哈希功能
    - 添加 init 命令初始化仓库
    
    - 实现 hash-object 命令计算文件 SHA1 值
    
    - 支持将对象存储到 .rgit/object 目录
    
    - 引入依赖：anyhow、clap、flate2、sha1
    
    - 添加 Cargo.lock 文件记录依赖版本

---
+++ src/commands.rs
@@ -0,0 +1,61 @@
+use std::io::Write;
+use std::{
+    fs::{self, read},
+    path::Path,
+};
+
+use flate2::{Compression, write::ZlibEncoder};
+use sha1::{Digest, Sha1};
+
+pub fn init() -> anyhow::Result<()> {
+    let root = Path::new(".rgit");
+    if root.exists() {
+        println!(
+            "Reinitialized existing rgit repository in {:?}",
+            fs::canonicalize("root")?
+        );
+    }
+    fs::create_dir_all(root.join("object"))?;
+    fs::create_dir_all(root.join("refs"))?;
+    fs::write(root.join("HEAD"), "ref: refs/heads/master\n")?;
+    println!(
+        "Initialized empty rgit repository in {:?}",
+        fs::canonicalize(root)?
+    );
+
+    Ok(())
+}
+pub fn hash_object(file_path: &Path, write: bool) -> anyhow::Result<String> {
+    let content = read(file_path)?;
+    let size = content.len();
+
+    let header = format!("blob {}\0", size);
+    let mut hasher = Sha1::new();
+    hasher.update(&header);
+    hasher.update(&content);
+    // let bytes: [u8; 20] = hasher.finalize().into();
+    let hash = <[u8; 20]>::from(hasher.finalize())
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>();
+    if write {
+        store_object(&hash, &header, &content)?;
+    }
+    Ok(hash)
+}
+fn store_object(hash: &str, header: &str, content: &[u8]) -> anyhow::Result<()> {
+    let rgit_dir = Path::new(".rgit/object");
+    let (dir, file) = hash.split_at(2);
+    let object_path = rgit_dir.join(dir);
+    fs::create_dir_all(&object_path)?;
+    let file_path = object_path.join(file);
+    if file_path.exists() {
+        return Ok(());
+    }
+    let f = fs::File::create(file_path)?;
+    let mut encoder = ZlibEncoder::new(f, Compression::default());
+    encoder.write_all(header.as_bytes())?;
+    encoder.write_all(content)?;
+    encoder.finish()?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+use self::commands::hash_object;
+mod commands;
+
+#[derive(Parser)]
+#[command(name = "rgit", about = "A mini-git implementation in Rust")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Subcommand)]
+enum Commands {
+    /// Initialize a new rgit repository
+    Init,
+    HashObject {
+        file: PathBuf,
+        #[arg(short = 'w')]
+        write: bool,
+    },
+}
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    match cli.command {
+        Commands::Init => {
+            commands::init()?;
+        }
+        Commands::HashObject { file, write } => {
+            let sha1 = hash_object(&file, write)?;
+            println!("{}", sha1);
+        }
+    }
+    Ok(())
 }
